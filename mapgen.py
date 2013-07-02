f8405e6d37449900c27ec8e15232e5da1b6f6512
Erty Seidel <ertysdl+github@gmail.com>
2013-07-02 17:18:55 -0400

    Created map generation, Ignored __pycache__

---
+++ mapgen.py
@@ -0,0 +1,24 @@
+import re
+import models
+
+def generateMap():
+    countries_file = open('./countries_graph.txt')
+    current_continent = None
+    current_country = None
+    themap = models.Map()
+    for line in countries_file:
+        split_line = re.split("\s", line)
+        if(split_line[0] != ''): # we are on a continent
+            current_continent = models.Continent(' '.join(split_line[:-2]), split_line[:-1])
+            themap.add_continent(current_continent)
+        else:
+            if(split_line[0] == '' and split_line[1] != ''): # we are on a country
+                current_country = models.Country(' '.join(split_line[1:-2]))
+                current_continent.countries.add(current_country)
+            else: #we are defining the relations
+                for c in split_line[3:-1]:
+                    current_country.border_countries.add(c)
+    return themap
+
+if __name__ == '__main__':
+    print(generateMap())
