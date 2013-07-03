f00dbda8b6c3adf67744bb11543ac2ae793ed946
Erik Taubeneck <erik.taubeneck@gmail.com>
2013-07-03 13:11:04 -0400

    updated test_risk.py

--- test_risk.py
+++ test_risk.py
@@ -3,35 +3,35 @@ import mapgen
 import unittest
 import random
 
+
+
 class TestBoard(unittest.TestCase):
     def setUp(self):
         self.board = mapgen.generate_board()
-        self.players = [models.Player('Erty'),models.Player('Alex')]
-        self.Alaska = board.countries['alaska']
-        self.Northwest_Territory = board.countries['northwest territory']
+                
+    def test_border_countries(self):
+        country_A = board.countries['alaska']
+        country_B = board.countries['northwest territory']
+        self.assertIn(country_A, country_B.border_countries)
+        self.assertIn(country_B, country_A.border_countries)
+
+    def test_not_border_countries(self):
+        country_A = board.countries['iceland']
+        country_B = board.countries['northwest territory']
+        self.assertIn(country_A, country_B.border_countries)
+        self.assertIn(country_B, country_A.border_countries)
         
+    def test_attack(self):
+        country_A = board.countries['alaska']
+        country_B = board.countries['northwest territory']
+        players = [models.Player('Erty'),models.Player('Alex')]
+        country_A.owner = players[0]
+        country_B = players[1]
+        country_A.troops = 30
+        country_B.troops = 10
+        with patch.object(random, 'randint') as mock_method:
+            mock_method.return_value = 6
+            country_A.attack(country_B, 3)
+            self.assertEqual(country_A, 28)
+            self.assertEqual(country_B, 10)
 
-board = mapgen.generate_board()
-players = []
-
-players.append(models.Player('Erty', None))
-players.append(models.Player('Alex', None))
-
-Alaska = board.countries['alaska']
-Northwest_Territory = board.countries['northwest territory']
-
-Alaska.add_troops(players[0], 30)
-Northwest_Territory.add_troops(players[1], 10)
-
-assert(Alaska.owner == players[0])
-assert(Northwest_Territory.owner == players[1])
-
-#while Alaska.owner != Northwest_Territory.owner and Alaska.troops > 1:
-#    Alaska.attack(Northwest_Territory, min(Alaska.troops, 3))
-#    print("Alaska: " + Alaska.owner.name + " : " + str(Alaska.troops))
-#    print("NWTerr: " + Northwest_Territory.owner.name + " : " + str(Northwest_Territory.troops))
-#    print("---")
-
-print(board.continents)
-
-print(board.continents['north america'].get_player_set())
