e61a528a4b5d4f026de6017e1bd1114bb50e08c0
Erik Taubeneck <erik.taubeneck@gmail.com>
2013-07-03 14:13:52 -0400

    updated test_board.py

--- test_risk.py
+++ test_risk.py
@@ -26,12 +26,19 @@ class TestBoard(unittest.TestCase):
         country_B = self.board.countries['northwest territory']
         players = [models.Player('Erty'),models.Player('Alex')]
         country_A.owner = players[0]
-        country_B = players[1]
+        country_B.owner = players[1]
         country_A.troops = 30
         country_B.troops = 10
         with patch.object(random, 'randint') as mock_method:
-            mock_method.return_value = 6
+            mock_method.side_effect = [6,6,1,1,1]
             country_A.attack(country_B, 3)
-            self.assertEqual(country_A, 28)
-            self.assertEqual(country_B, 10)
+            self.assertEqual(country_A.troops, 28)
+            self.assertEqual(country_B.troops, 10)
+
+
+        with patch.object(random, 'randint') as mock_method:
+            mock_method.side_effect = [1,1,6,6,6]
+            country_A.attack(country_B,3)
+            self.assertEqual(country_A.troops, 28)
+            self.assertEqual(country_B.troops, 8)
 
