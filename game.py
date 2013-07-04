2d247cfb9870338ea995318059167beda6c1424e
Erik Taubeneck <erik.taubeneck@gmail.com>
2013-07-04 12:47:19 -0400

    added init_deploy fuction to Game object

--- game.py
+++ game.py
@@ -1,32 +1,42 @@
 from boardgen import generate_board
 from uuid import uuid4
 import random
+import itertools
 
 class Game(object):
 
     def __init__(self, players_list):
         self.phases = ['place','attack','reinforce']
         self.board = generate_board()
-        self.players_list = list(players_list)
-        random.shuffle(self.players_list)
+        self.player_list = random.shuffle(list(player_list))
         self.uid = uuid4()
 
+
+    def start_game(self):
         #assign countries to players
-        self.players = self.player_gen()
-        for country in self.board.countries:
-            self.players.next().countries.add(country)
+        self.init_deploy(player_list)
+        
 
         self.current_phase_index = 0
         self.players = self.player_gen() # reset players to start with p1
         self.current_player = self.players.next()
 
-    def player_gen(self):
-        index = 0
-        while(True): 
-            if index >= len(self.players_list):
-                index = 0
-            yield self.players_list[index]
-            index += 1
+
+    def init_deploy(player_list):
+        players = itertools.cycle(self.player_list)
+        while {c for c in self.board.countries if not c.owner}:
+            player = players.next()
+            player.choose_country(self.board)
+
+        initial_troops = {3:35,
+                          4:30,
+                          5,25,
+                          6,20}
+
+        for _ in xrange(len(player_list) * initial_troops(len(player_list))):
+            player = players.next()
+            player.deploy_troop(self.board)
+    
 
     def finish_turn(self):
         self.current_player = self.players.next()
