f7647dfb91926f8a82e745d680ed49a073b2c0e8
Alex <wilson21@u.washington.edu>
2013-07-08 11:50:45 -0400

    expanded add_troops test

--- test_risk.py
+++ test_risk.py
@@ -23,11 +23,16 @@ class TestBoard(unittest.TestCase):
     
     def test_add_troops(self):
         country_A = self.board.countries['iceland']
-        players = [models.Player('Erty')]
+        country_B = self.board.countries['northwest territory']
+        players = [models.Player('Erty'), models.Player('Alex')]
         country_A.owner=players[0]
+        country_B.owner=players[1]
         country_A.troops=20
+        country_B.troops=20
         country_A.add_troops(players[0], 10)
+        country_B.add_troops(players[0], 10)
         self.assertEqual(country_A.troops, 30)
+        self.assertEqual(country_B.troops, 20)
 
     def test_attack(self):
         country_A = self.board.countries['alaska']
@@ -49,4 +54,11 @@ class TestBoard(unittest.TestCase):
             country_A.attack(country_B,3)
             self.assertEqual(country_A.troops, 28)
             self.assertEqual(country_B.troops, 8)
-
+    
+    def test_get_player_set(self):
+        country_A = self.board.countries['alaska']
+        country_B = self.board.countries['northwest territory']
+        players = [models.Player('Erty'),models.Player('Alex')]
+        country_A.owner = players[0]
+        country_B.owner = players[1]
+        
