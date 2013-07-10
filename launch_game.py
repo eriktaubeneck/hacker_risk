0fdbeae18327106a83104a1604b0c7aa8fc73c35
Erty Seidel <ertysdl+github@gmail.com>
2013-07-10 16:47:08 -0400

    Added dummy ai urls

--- launch_game.py
+++ launch_game.py
@@ -3,11 +3,11 @@ from risk.models import Players
 from risk.game import Game
 
 player1_name = 'player 1'
-player1_url = 'url1 goes here'
+player1_url = 'http://localhost:4444'
 player2_name = 'player 2'
-player2_url = 'url2 goes here'
+player2_url = 'http://localhost:4445'
 player3_name = 'player 3'
-player3_url = 'url3 goes here'
+player3_url = 'http://localhost:4446'
 
 players = Players([Player(player1_name, player1_url),
                    Player(player2_name, player2_url),
