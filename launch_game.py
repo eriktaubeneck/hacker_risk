20f073d7011340757f88cf0d67cdfdd7fb93963c
Erik Taubeneck <erik.taubeneck@gmail.com>
2013-07-16 17:37:25 -0400

    actually increment the port

--- launch_game.py
+++ launch_game.py
@@ -8,7 +8,7 @@ first_port = 4444
 
 players = Players()
 for i in range(n_players):
-    players.add_player(Player('player %s' % i, 'http://localhost:%s' % (first_port)))
+    players.add_player(Player('player %s' % i, 'http://localhost:%s' % (first_port+1)))
 
 game = Game(players)
 
