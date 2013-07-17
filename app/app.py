d1bff489c12ad324a291df2d81513d3b4cc7c1df
Erik Taubeneck <erik.taubeneck@gmail.com>
2013-07-17 14:58:58 -0400

    added user model, updated templates

--- app/app.py
+++ app/app.py
@@ -1,17 +1,47 @@
 from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.wtf import Form
+from flask.ext.wtf.html5 import URLField
+from wtforms import TextField
+import os
 from risk_helper import Player
 from risk.models import Players
 from risk.game import Game
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or ('sqlite:///' + os.path.join(app.root_path, '../app.db'))
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    base_url = db.Column(db.String(200), unique=True)
+
+    def __init__(self, username, base_url):
+        self.username = username
+        self.base_url = base_url
+
+    def __repr__(self):
+        return '<User %r @ %r>' % (self.username, self.base_url)
+
+class UserForm(Form):
+    username = TextField("Username")
+    base_url = URLField("AI URL")
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/add_player")
-def add_player():
-    return render_template("add_player.html")
+@app.route("/signup", methods=("GET", "POST"))
+def signup():
+    form = UserForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        base_url = form.base_url.data
+        user = User(username, base_url)
+        db.session.add(user)
+        db.session.commit()
+    return render_template("signup.html")
 
 @app.route("/start_game")
 def start_game():
