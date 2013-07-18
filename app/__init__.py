65a02b7d84f3526cbfdd0ef690afd6f3d04c6782
Erik Taubeneck <erik.taubeneck@gmail.com>
2013-07-17 20:57:41 -0400

    implemented start-game route

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,12 @@ from flask import Flask, render_template, flash
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.wtf import Form
 from flask.ext.wtf.html5 import URLField
-from wtforms import TextField
+from wtforms import TextField, SelectMultipleField
 import os
 from risk_helper import Player
 from risk.models import Players
 from risk.game import Game
+from start_game import start_risk_game
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or ('sqlite:///' + os.path.join(app.root_path, '../app.db'))
@@ -29,6 +30,9 @@ class UserForm(Form):
     username = TextField("Username")
     base_url = URLField("AI URL")
 
+class StartGameForm(Form):
+    players = SelectMultipleField("Select Players")
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -45,14 +49,20 @@ def signup():
         form = UserForm()
     return render_template("signup.html", form=form)
 
-@app.route("/start_game")
+@app.route("/start-game", methods=("GET", "POST"))
 def start_game():
-    player_list = [] # Need to load this from form arguments.
-    players = Players()
-    for player_info in player_list: # player_info is tuple (player_name, player_url).
-        player = Player(player_info[0], player_info[1])
-        Players.add_player(player)
-    game = Game(players)
-    Game.start_game()
-
-
+    form = StartGameForm()
+    form.players.choices = [(str(user.id), user.username) for user in User.query.all()]
+    if form.validate_on_submit():
+        print 'got here;'
+        user_ids = list(form.players.data)
+        if 3 <= len(user_ids) <= 6:
+            print user_ids
+            users = [(user.username, user.base_url) for user in
+                     User.query.filter(User.id.in_(user_ids)).all()]
+            start_risk_game.delay(users)
+        elif len(user_ids) < 3:
+            flash("you need at least 3 players to start a game")
+        else:
+            flash("you cannont play with more than 6 players")
+    return render_template("start-game.html", form=form)
