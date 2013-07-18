aa6d6edb26383bdfb0dbf55f1d0476d48525c3bc
Erik Taubeneck <erik.taubeneck@gmail.com>
2013-07-18 14:35:15 -0400

    added Game model in DB

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,13 @@
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, redirect, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.pymongo import PyMongo
 from flask.ext.wtf import Form
 from flask.ext.wtf.html5 import URLField
 from wtforms import TextField, SelectMultipleField
 import os
-from risk_helper import Player
-from risk.models import Players
-from risk.game import Game
-from start_game import start_risk_game
+import json
+from risk.game import Game as RiskGame
+from app.forms import UserForm, StartGameForm
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or ('sqlite:///' + os.path.join(app.root_path, '../app.db'))
@@ -16,24 +15,9 @@ app.secret_key = 'f520d319-8b73-45c1-9982-07e57c0ddaa6'
 db = SQLAlchemy(app)
 mongo = PyMongo(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    base_url = db.Column(db.String(200), unique=True)
-
-    def __init__(self, username, base_url):
-        self.username = username
-        self.base_url = base_url
-
-    def __repr__(self):
-        return '<User %r @ %r>' % (self.username, self.base_url)
-
-class UserForm(Form):
-    username = TextField("Username")
-    base_url = URLField("AI URL")
-
-class StartGameForm(Form):
-    players = SelectMultipleField("Select Players")
+from app.models import User, Game
+from risk_helper import Player, Players
+from start_game import start_risk_game
 
 @app.route("/")
 def index():
@@ -56,13 +40,17 @@ def start_game():
     form = StartGameForm()
     form.players.choices = [(str(user.id), user.username) for user in User.query.all()]
     if form.validate_on_submit():
-        print 'got here;'
         user_ids = list(form.players.data)
         if 3 <= len(user_ids) <= 6:
-            print user_ids
-            users = [(user.username, user.base_url) for user in
-                     User.query.filter(User.id.in_(user_ids)).all()]
-            start_risk_game.delay(users)
+            game = Game()
+            for user_id in user_ids:
+                user = User.query.filter(User.id == user_id).one()
+                game.users.append(user)
+            db.session.add(game)
+            db.session.commit()
+            start_risk_game.delay(user_ids, game.id)
+            flash("Game added to Queue")
+            redirect(url_for('index'))
         elif len(user_ids) < 3:
             flash("you need at least 3 players to start a game")
         else:
--- start_game.py
+++ start_game.py
@@ -1,18 +1,31 @@
 from celery import Celery
-from risk.models import Players
-from risk_helper import Player
-from risk.game import Game
-
+from risk.game import Game as RiskGame
 
 celery = Celery('tasks', backend='amqp', broker='amqp://guest@localhost//')
 
 @celery.task
-def start_risk_game(user_ids):
+def start_risk_game(user_ids, game_id):
     players = Players()
     for user_id in user_ids:
         user = User.query.filter(User.id == user_id).one()
         players.add_player(Player(user.username, user.base_url))
-    game = Game(players)
-    game.start_game()
+    risk_game = RiskGame(players)
+    game = db.session.query(Game).filter(Game.id == game_id).one()
+    print game_id
+    print game
+    game.uid = str(risk_game.uid)
+    game.is_running = True
+    db.session.add(game)
+    db.session.commit()
+    risk_game.start_game()
+    game.is_running = False
+    game.completed = True
+    if risk_game.winner:
+        game.winner = User.query.filter(User.username == risk_game.winner).one()
+    game.broadcast_count = risk_game.broadcast_count
+    db.session.add(game)
+    db.session.commit()
 
-from app.models import User
+from app import db
+from app.models import User, Game
+from risk_helper import Player, Players
